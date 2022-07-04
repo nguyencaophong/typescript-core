faf8964b93bbde8f3317593d3d128bc8d6e37b4c
Root <phongnguyenw@gmail.com>
2022-07-04 22:34:20 +0700

    setup env typescript

---
+++ app.ts
@@ -0,0 +1,23 @@
+// const myFucn = (name: string, ...arrName: string[])=>{
+//     return name+' '+ arrName.join(',')+'!!!'
+// }
+
+// const resultl = myFucn('Xin Chao','Nguyen','Cao','Phong')
+
+
+
+// console.log(resultl);
+
+interface Employee { 
+    name: string; 
+    code: number; 
+} 
+
+let employee = <Employee> { }; 
+employee.name = "John"; 
+employee.code = 123; 
+
+
+
+
+
